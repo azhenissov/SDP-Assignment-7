6dded5519aec71782da30f80cfb29b7c087c3049
Anuar <zhenisovanuar@gmail.com>
2025-10-30 15:24:20 +0500

    small changes

--- WeatherApp.java
+++ WeatherApp.java
@@ -13,5 +13,8 @@ public class WeatherApp {
         station.setMeasurements(25.3f, 65.0f, 1012.5f);
         System.out.println("-------------");
         station.setMeasurements(26.7f, 60.0f, 1010.2f);
+
+        station.registerObserver(phone);
+        station.registerObserver(window);
     }
 }
